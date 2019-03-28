d16c21b9dc53f59f5db45f01213b8da67e1677a8
needMoreBlood <fedulova.lena2051@gmail.com>
2019-03-28 19:54:15 +0500

    реализована Хэш-таблицу

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hashTable;
+
+namespace hashtable
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var check = new HashTable(3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                check.PutPair(i, i);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(check.GetValueByKey(i), i);
+            }
+        }
+
+        [TestMethod]
+        public void EquialElements()
+        {
+            var check = new HashTable(2);
+
+            check.PutPair("a", 3232);
+            check.PutPair("a", 342);
+
+            Assert.AreEqual(check.GetValueByKey("a"), 342);
+        }
+
+        [TestMethod]
+        public void TenHElements()
+        {
+            var check = new HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                check.PutPair(i, i);
+            }
+
+            Assert.AreEqual(check.GetValueByKey(1), 1);
+        }
+
+        [TestMethod]
+        public void TenHElements2()
+        {
+            var check = new HashTable(11000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                check.PutPair(i, i + 1);
+            }
+
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(check.GetValueByKey(i), null);
+            }
+        }
+    }
+}
--- hashTable/Program.cs
+++ hashTable/Program.cs
@@ -6,10 +6,71 @@ using System.Threading.Tasks;
 
 namespace hashTable
 {
-    class Program
+    public class HashTable
+
     {
-        static void Main(string[] args)
+        KeyValue[] hash;
+
+        public static void Main() { }
+
+        public HashTable(int size)
+
         {
+            hash = new KeyValue[size];
+        }
+
+        private int GetPosition(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % hash.Length;
+        }
+
+        public void PutPair(object key, object value)
+
+        {
+            var position = GetPosition(key);
+
+            if (hash[position] is null)
+                hash[position] = new KeyValue(key, value);
+            else
+            {
+                var current = hash[position];
+                if (current.Key.Equals(key))
+                {
+                    current.Value = value;
+                    return;
+                }
+            }
+        }
+
+        public object GetValueByKey(object key)
+
+        {
+            var position = GetPosition(key);
+
+            if (hash[position] is null)
+                return null;
+
+            else
+            {
+                var curent = hash[position];
+                if (curent.Key.Equals(key))
+                {
+                    return curent.Value;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class KeyValue
+    {
+        public object Key { get; set; }
+        public object Value { get; set; }
+        public KeyValue(object key, object value)
+        {
+            Key = key;
+            Value = value;
         }
     }
 }
